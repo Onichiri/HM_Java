83fd8a1382731229cbdd969a0c79e90629715ddf
Onichi <99264476+Onichiri@users.noreply.github.com>
2022-02-13 14:56:00 +0300

    Delete HM_2_1.java

--- HM_2_1.java
+++
@@ -1,9 +0,0 @@
-class HM_2_1 {
-    public static void main(String[] args) {
-        System.out.println(between10and20(15, 8));
-    }
-
-    static boolean between10and20(int a, int b) {
-        return a + b >= 10 & a + b <= 20;
-    }
-}
