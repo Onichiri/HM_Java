ad75de5e2221d9e2286a6118e897b91e05962927
Onichi <99264476+Onichiri@users.noreply.github.com>
2022-02-13 14:56:06 +0300

    Delete HM_2_2.java

--- HM_2_2.java
+++
@@ -1,7 +0,0 @@
-class HM_2_2 {
-    public static void main(String[] args) {
-        int a = 0;
-        String isPositiveOrNegative = (a >= 0) ? "Число положительное" : "Число отрицательное";
-        System.out.println(isPositiveOrNegative);
-    }
-}
