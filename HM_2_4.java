bc8d90a8bcdb381ff719d1128cf67cc25c87762d
Onichi <99264476+Onichiri@users.noreply.github.com>
2022-02-13 14:56:17 +0300

    Delete HM_2_4.java

--- HM_2_4.java
+++
@@ -1,8 +0,0 @@
-class HM_2_4 {
-    public static void main(String[] args) {
-        String text = "Meow";
-        for (int c = 5; c > 0; c--) {
-            System.out.println(text);
-        }
-    }
-}
